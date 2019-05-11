b44b5a74a841c34378881c97dbd015c563081f31
Silvana Pesenti <silvana_pesenti@hotmail.com>
2019-05-11 11:53:42 +0100

    polishing code
    temporary function outside stress_VaR
    spacing

--- R/stress_VaR.R
+++ R/stress_VaR.R
@@ -1,6 +1,6 @@
  #' Stressing Value-at-Risk
  #' 
- #' Provides the scenario weights such that the random variable
+ #' Provides scenario weights such that the random variable
  #'    under the new scenraio weights fulfils the constraint on the VaR and
  #'    has minimal Kullback-Leibler divergence to the baseline random
  #'    variable.
@@ -43,49 +43,51 @@
  #' @export
  #' 
   stress_VaR <- function(x, alpha, q_ratio = NULL, q = NULL, k = 1){
-   if(is.SWIM(x)) x_data <- get.data(x) else x_data <- as.matrix(x)
-   if(anyNA(x_data)) warning("'x' contains NA")
-   if(any(alpha <= 0) || any(alpha >= 1)) stop("invalid 'alpha' argument")
-   if(!is.null(q) && !is.null(q_ratio)) stop("only provide 'q' or 'q_ratio'")
-   if(is.null(q) && is.null(q_ratio)) stop("no stress defined")
+   if (is.SWIM(x)) x_data <- get.data(x) else x_data <- as.matrix(x)
+   if (anyNA(x_data)) warning("'x' contains NA")
+   if (any(alpha <= 0) || any(alpha >= 1)) stop("invalid 'alpha' argument")
+   if (!is.null(q) && !is.null(q_ratio)) stop("only provide 'q' or 'q_ratio'")
+   if (is.null(q) && is.null(q_ratio)) stop("no stress defined")
 
-   ## x_data[, k] is the component of x_data that is stressed
+   ## x_data[, k] component of x_data that is stressed
    n <- length(x_data[, k])
    VaR <- quantile(x_data[, k], alpha, names = FALSE, type = 1)
 
    if(is.null(q)){
-      if(!is.numeric(q_ratio)) stop("invalid 'q_ratio' argument")
-      if(any(VaR == 0)) warning("VaR is 0, define 'q' instead of 'q_ratio'")
-      if(length(alpha) > 1 && length(q_ratio) > 1 && length(alpha) != length(q_perc)) stop("arguments 'alpha' and 'q_ratio' must have length one or equal length")
+      if (!is.numeric(q_ratio)) stop("invalid 'q_ratio' argument")
+      if (any(VaR == 0)) warning("VaR is 0, define 'q' instead of 'q_ratio'")
+      if (length(alpha) > 1 && length(q_ratio) > 1 && length(alpha) != length(q_perc)) stop("arguments 'alpha' and 'q_ratio' must have length one or equal length")
       max_length <- max(length(q_ratio), length(alpha))
       q <- q_ratio * VaR
    } else {
-      if(!is.numeric(q)) stop("invalid 'q' argument")
-      if(length(alpha) > 1 && length(q) > 1 && length(alpha) != length(q)) stop("arguments 'alpha' and 'q' must have length one or equal length")
+      if (!is.numeric(q)) stop("invalid 'q' argument")
+      if (length(alpha) > 1 && length(q) > 1 && length(alpha) != length(q)) stop("arguments 'alpha' and 'q' must have length one or equal length")
       max_length <- max(length(q), length(alpha))
    }  
    q <- rep(q, length.out = max_length)  
    alpha <- rep(alpha, length.out = max_length)
 
    ## check if VaR < q < ess sup (x_data)
-   if(any(VaR > q)) print("VaR > q, quantile constraint is interpreted as probability constraint.")
-   if(any(q > VaR & ecdf(x_data[, k])(VaR) == ecdf(x_data[, k])(q))) stop("There are not enough data points, specifically, there is none between VaR and q.")
-   if(any(q >= max(x_data[, k])) || any(q <= min(x_data[, k]))) stop("all q need to be smaller than the largest data point and bigger than the smallest data point.") 
+   if (any(VaR > q)) print("VaR > q, quantile constraint interpreted as probability constraint.")
+   if (any(q > VaR & ecdf(x_data[, k])(VaR) == ecdf(x_data[, k])(q))) stop("There are not enough data points, specifically, there is none between VaR and q.")
+   if (any(q >= max(x_data[, k])) || any(q <= min(x_data[, k]))) stop("all q need to be smaller than the largest data point and bigger than the smallest data point.") 
 
-    # the temporary function 
-    .rn_VaR <- function(y, constraints){
-    constraints <- as.numeric(constraints)
-    prob_q <- mean(y < constraints[2])
-    rn_weights <- function(z)(constraints[1] / prob_q) * (z < constraints[2]) + (1 - constraints[1]) / (1 - prob_q) * (z >= constraints[2])
-      return(rn_weights)
-    }
-    constr = cbind(alpha, q)
+    constr <- cbind(alpha, q)
     new_weights <- apply(X = constr, MARGIN = 1, FUN = .rn_VaR, y = x_data[, k])
-    if(is.null(colnames(x_data))) colnames(x_data) <-  paste("X", 1 : ncol(x_data), sep = "")
-    names(new_weights) <- paste("stress", 1 : max_length)
+    if (is.null(colnames(x_data))) colnames(x_data) <-  paste("X", 1:ncol(x_data), sep = "")
+    names(new_weights) <- paste("stress", 1:max_length)
     specs <- data.frame("type" = rep("VaR", length.out = max_length), "k" = rep(k, length.out = max_length), constr, stringsAsFactors = FALSE)
-    rownames(specs) <- paste("stress", 1 : max_length)
+    rownames(specs) <- paste("stress", 1:max_length)
     my_list <- SWIM("x" = x_data, "new_weights" = new_weights, "specs" = specs)
-   if(is.SWIM(x)) my_list <- merge(x, my_list)
+   if (is.SWIM(x)) my_list <- merge(x, my_list)
   return(my_list)
+  }
+  
+  # help function 
+  .rn_VaR <- function(y, constraints){
+     .alpha <- as.numeric(constraints[1])
+     .q <- as.numeric(constraints[2])
+     prob_q <- mean(y < .q)
+     rn_weights <- function(z)(.alpha / prob_q) * (z < .q) + (1 - .alpha) / (1 - prob_q) * (z >= .q)
+     return(rn_weights)
   }
