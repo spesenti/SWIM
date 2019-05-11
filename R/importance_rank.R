18cde6a56ef5823ffd66b702413c40f6d1b1adbe
Silvana Pesenti <silvana_pesenti@hotmail.com>
2019-05-11 14:30:25 +0100

    polishing code

--- R/importance_rank.R
+++ R/importance_rank.R
@@ -6,18 +6,17 @@
 # type      character vector, c("Gamma", "Wasserstein", "all"). The Kolmogorov distance is the same for all inputs.
 # f         list of functions, calcualted the sensitivity of the transformed input vector. List needs to have the same length as xCol. 
 
-importance_rank <- function(x, xCol = "all", wCol = "all", type = c("Gamma", "Wasserstein", "all"), f = NULL){
-  if(!is.SWIM(x)) stop("wrong object")
-  if(anyNA(x$x)) warning("'x' contains NA")
-  if(missing(type)) type <- "all"
+  importance_rank <- function(x, xCol = "all", wCol = "all", 
+    type = c("Gamma", "Wasserstein", "all"), f = NULL){
+   if (!is.SWIM(x)) stop("Wrong object")
+   if (anyNA(x$x)) warning("x contains NA")
+   if (missing(type)) type <- "all"
   
-  sens_w <- sensitivity(x, xCol = xCol, wCol = wCol, type = type, f = f)
-  if(length(sens_w) < 4) stop("only one input provided.")
-  rank_w <- t(apply(X = sens_w[, 1:(length(sens_w)-2)], FUN = function(z) rank(z, ties.method = "min"), MARGIN = 1))
-  rank_w <- cbind(rank_w, sens_w[, (length(sens_w)-1):length(sens_w)])
+   sens_w <- sensitivity(x, xCol = xCol, wCol = wCol, type = type, f = f)
+   if (length(sens_w) < 4) stop("Only one input provided.")
+   rank_w <- t(apply(X = sens_w[ , 1:(length(sens_w) - 2)], MARGIN = 1, FUN = function(z) rank(z, ties.method = "min")))
+   rank_w <- cbind(rank_w, sens_w[ , (length(sens_w) - 1):length(sens_w)])
     
-  if(is.character(type) && type == "all") rank_w <- rank_w[-which(rank_w[,"type"] == "Kolmogorov"), ]
-  
-  return(rank_w)
-}
-
+   if (is.character(type) && type == "all") rank_w <- rank_w[-which(rank_w[,"type"] == "Kolmogorov"), ]
+   return(rank_w)
+  }
