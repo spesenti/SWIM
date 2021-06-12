631504518ef7cde2062e6b0578d17eb3cefe1de3
Kent Wu <kentwhf@gmail.com>
2021-06-12 18:23:02 +0800

    cdf add base and vector input

--- R/cdf.R
+++ R/cdf.R
@@ -24,9 +24,7 @@
 #'   alpha = c(0.9, 0.95), q_ratio = 1.05)
 #' grid <- seq(min(x$normal), max(x$normal), length.out = 5)
 #' ## stressed empirical distribution function
-#' cdf(res1, xCol = 1, wCol = 1)(grid)
-#' ## baseline empirical distribution function
-#' ecdf(x$normal)(grid)
+#' cdf(res1, xCol = 1, wCol = 1, grid = grid)
 #' 
 #' @author Silvana M. Pesenti 
 #' 
@@ -35,21 +33,43 @@
 #'     \code{\link{quantile_stressed}} for sample quantiles of 
 #'     a stressed model. 
 #' @export
+#' 
 
-  cdf <- function(object, xCol = 1, wCol = 1){
-   if (!is.SWIM(object)) stop("Object not of class 'SWIM'")
-   if (anyNA(object$x)) warning("x contains NA")
-   new_weights <- get_weights(object)[ , wCol]
-   x_data <- get_data(object)[ , xCol]
-   cdf <- .cdf(x = x_data, w = new_weights)
-   return(cdf)
-  }
+  cdf <- function(object, xCol = "all", wCol = 1, grid, base=FALSE){
+    if (!is.SWIM(object)) stop("Object not of class 'SWIM'")
+    if (anyNA(object$x)) warning("x contains NA")
+    if (is.null(colnames(get_data(object)))){
+      cname <-  paste("X", as.character(xCol), sep = "")
+    } else if (!is.character(xCol)){
+      cname <- colnames(get_data(object))[xCol]
+    } else {
+      cname <- xCol   
+    }
+    new_weights <- as.matrix(get_weights(object)[ , wCol]) # single weight
+    x_data <- as.matrix(get_data(object)[ , xCol])
+    cdf <- .cdf(x = x_data, w = new_weights, grid = grid)
+    colnames(cdf) <- cname
+        
+    if (base == TRUE){
+      old_weights <- as.matrix(rep(1, length(x_data[,1])), ncol = 1)
+      cdf_base <- .cdf(x = x_data, w = old_weights, grid = grid)
+      colnames(cdf_base) <- paste("base", cname, sep=' ')
+      cdf <- cbind(cdf, cdf_base)
+    }
+    return(cdf)
+    }
+  
   # tst 
-
- # help function 
- # x    numeric vector  
- # w    numeric vector with weights
-  .cdf <- function(x, w){
-    .cdf_stress <- function(t) colMeans(w * (sapply(t, FUN = function(s) x <= s)))
-   return(.cdf_stress)
-  }
+  
+  # help function 
+  # x    numeric vector  
+  # w    numeric vector with weights
+  .cdf <- function(x, w, grid){
+    d <- ncol(x)
+    t <- as.matrix(grid)
+    
+    res <- matrix(NA, ncol=d, nrow=ncol(t))
+    for (i in 1:d) {res[, i] <- t(w) %*% sapply(t[i, ], FUN = function(s) x[, i] <= s) / nrow(x)}
+    return(res)
+    }
+    
